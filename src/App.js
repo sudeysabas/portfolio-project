19a2f8f3de029d7550884f3852fc618ec6376777
sudeysabas.com <sudeysabas10@gmail.com>
2021-08-05 16:56:16 -0400

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home.js';
+
+function App() {
+  return (
+    <div className="App">
+        <Home />
+        <h1>Portfolio project ....</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+function Home(){
+    return(
+        <h1>hello world</h1>
+    );
+}
+
+export default Home;
